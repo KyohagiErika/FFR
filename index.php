0f60af28abf4d5c5d8561cd7325bdffb946022d1
MinhhTien <minhtienn1209@gmail.com>
2022-04-15 20:20:02 +0700

    Update file index.php,info.php

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 require './config/app_info.php';
 require './engine/processor/uri_handler.php';
 require './config/router.php';
+require './view/head.php';
+require './view/wow_js.php';
+require './view/navbar_section.php';
 require $PAGE_URI;
+require './view/footer_section.php';
+require './view/back_to_top.php';
+require './view/all_scripts_index.php';
