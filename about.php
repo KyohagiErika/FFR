e974b0a5de4def6a4d4538c31e653cf18906f665
linhlinh38 <99740575+linhlinh38@users.noreply.github.com>
2022-04-17 13:42:29 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+require './config/app_info.php';
+require './engine/processor/uri_handler.php';
+require './config/router.php';
+require 'view/head.php';
+require 'view/navbar.php';
+require 'view/bannersection.php';
+require 'view/aboutsection.php';
+require 'view/teamsection.php';
+require 'view/footer.php';
+require 'view/backtotop.php';
+require 'view/allscript.php';
+
+?>
---
+++ attendance.php
@@ -0,0 +1,13 @@
+<?php
+require './config/app_info.php';
+require './engine/processor/uri_handler.php';
+require './config/router.php';
+require 'view/head.php';
+require 'view/navbar.php';
+require 'view/bannersection.php';
+require 'view/camerasection.php';
+require 'view/footer.php';
+require 'view/backtotop.php';
+require 'view/allscript.php';
+
+?>
