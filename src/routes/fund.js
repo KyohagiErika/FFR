493ff9bbb006f2d3653c7292965f7238cb225a13
Lê Thành Long <48479168+KyohagiErika@users.noreply.github.com>
2022-05-22 14:46:58 +0700

    Update fund v2

--- src/routes/fund.js
+++ src/routes/fund.js
@@ -1,7 +1,12 @@
 const routes = require('express').Router()
 
 routes.get('/', (req, res, next) => {
-    res.status(200).send('Hello Fund!')
+    const data = {
+        name: "Long",
+        age: 19
+    }
+    res.setHeader('Content-Type', 'application/json')
+    res.send(data)
 })
 
 module.exports = routes
