cffc8da805b6c071adb049234e48f29536339925
Phạm Công Minh <92376692+ListKelvin@users.noreply.github.com>
2022-06-06 22:35:07 +0700

    Add files via upload

---
+++ public/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      dropShadow:{
+        '2xl': '0 25px 25px rgba(0, 105, 172, 0.25)'
+      },
+     
+    },
+  },
+  plugins: [],
+}
