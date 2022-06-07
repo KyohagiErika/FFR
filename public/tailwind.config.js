e2ca777eb63b0166949f96686de0c6e27e316c81
ListKelvin <phamm5687@gmai.com>
2022-06-07 15:36:33 +0700

    minh

---
+++ public/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      dropShadow:{
+        '2xl': '0 25px 25px rgba(0, 105, 172, 0.25)'
+      },
+      // backgroundImage: {
+      //   'hero':"url('/media/hero/31620322.png')"
+      // },
+     
+    },
+  },
+  plugins: [],
+}
