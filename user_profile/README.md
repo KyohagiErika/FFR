4d93106425e8a46fc62dc9037a228baaa9e8c4db
ngockim109 <ngockim9047@gmail.com>
2022-06-09 00:25:01 +0700

    user_profile

---
+++ user_profile/README.md
@@ -0,0 +1,1 @@
+# ai_app_fe_user
---
+++ user_profile/user-profile.js
@@ -0,0 +1,9 @@
+document.getElementById('save').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
