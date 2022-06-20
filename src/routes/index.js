bd79b1414bd67cf1b33e853c1301624ec0186c11
MinhhTien <minhtienn1209@gmail.com>
2022-06-20 12:45:13 +0700

    resovle conflict in routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,35 @@ const fs = require('fs/promises')
 const routes = require('express').Router()
 
 routes.get('/test', (req, res, next) => {
-    res.render('test', { layout: 'test' });
+    res.render('test', { layout: 'test' })
 })
+//routes.get('/test', studentMiddleware.getStudent)
+// routes.post('/test', upload.uploadImage.single('img'), async (req, res, next) => {
+//     const file = req.file
+//     if (file) {
+//         if (file.mimetype.split('/')[0] !== 'image') {
+//             await fs.unlink(file.path)
+//             if (req.xhr) {
+//                 res.status(400).send('Your uploaded file is not an image!')
+//             } else {
+//                 res.render('test', { layout: 'test', message: 'Your uploaded file is not an image!' })
+//             }
+//         } else {
+//             const realPath = file.path.replace('public', '')
+//             if (req.xhr) {
+//                 res.send(realPath)
+//             } else {
+//                 res.render('test', { layout: 'test', imgPath: realPath })
+//             }
+//         }
+//     } else {
+//         if (req.xhr) {
+//             res.status(400).send('Empty sent data!')
+//         } else {
+//             res.render('test', { layout: 'test', message: 'Empty sent data!' })
+//         }
+//     }
+// })
 routes.get('/', indexMiddleware.renderHome)
 routes.get('/about', indexMiddleware.renderAbout)
 routes.get('/logout', auth.studentOnly, indexMiddleware.logout)
@@ -17,6 +44,9 @@ routes.use('/account', require('./account'))
 routes.use('/attendance', require('./attendance'))
 routes.use('/fund', require('./fund'))
 routes.use('/admin', require('./admin'))
+routes.use('/user', require('./user'))
+routes.use('/camera', require('./camera'))
+routes.use('/ai', require('./ai'))
 routes.use('/user-attendance', require('./user-attendance'))
 routes.use('/user-fund', require('./user-fund'))
 routes.use('/user-profile', require('./user-profile'))
